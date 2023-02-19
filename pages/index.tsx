b5e50bcb88c66ab65d7d0c5e95c118ccc1f92f85
UnloadingGnat <jaavinjm123@gmail.com>
2023-02-19 17:49:35 -0500

    Button fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -106,11 +106,13 @@ export default function Home() {
             <h2 className="text-black text-2xl font-medium">Jaavin Mohanakumar</h2>
             <h3 className="text-black text-base font-medium pb-2 border-b-2">Founder and President</h3>
             <p className="mt-2 text-[#817c73] text-base">Jaavin Mohanakumar is the Founder and President of McMaster Blockchain Club. He is an Engineering 1 student at McMaster University, and has previously worked at Huff, the largest independent Ethereum assembly language.</p>
-            <button
-              className="rounded-full border w-10 border-[#0173af] bg-[#0172af89] p-1.5 px-2 text-sm text-white transition-all hover:bg-[#0173af] hover:text-black mt-2"
-            >
-              <a href="https://www.linkedin.com/in/jaavinmohanakumar/" target="_blank" rel="noreferrer"><Google className="h-5 w-5 text-white" /></a>
-            </button>
+            <a href="https://www.linkedin.com/in/jaavinmohanakumar/" target="_blank" rel="noreferrer">
+              <button
+                className="rounded-full border w-10 border-[#0173af] bg-[#0172af89] p-1.5 px-2 text-sm text-white transition-all hover:bg-[#0173af] hover:text-black mt-2"
+              >
+                <Google className="h-5 w-5 text-white" />
+              </button>
+            </a>
           </div>
 
           <div className=" p-4  md:h-56 flex flex-col gap-1">

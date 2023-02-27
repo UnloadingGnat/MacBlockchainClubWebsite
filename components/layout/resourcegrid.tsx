17113966773eaa5815d8f0b61b941951c38ae38a
UnloadingGnat <jaavinjm123@gmail.com>
2023-02-27 01:08:55 -0500

    Add new resources

--- components/layout/resourcegrid.tsx
+++ components/layout/resourcegrid.tsx
@@ -9,11 +9,11 @@ export default function ResourceGrid() {
                 <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-x-4 gap-y-8 w-full">
                     {resources.map(({ title, description, link, linkText }) => (
                         <a key={title}>
-                            <div className="rounded-2xl border-2 border-gray-100 overflow-hidden shadow-sm bg-white hover:shadow-md hover:translate-y-[-0.25rem] transition-all ease-out duration-200">
-                                    <div className="py-8 px-5 h-55 border-t border-gray-200">
+                            <div className="rounded-2xl border-2 border-gray-200 overflow-hidden shadow-sm bg-white hover:shadow-md hover:translate-y-[-0.25rem] transition-all ease-out duration-200">
+                                    <div className="py-8 px-5 h-55">
                                         <h3 className="font-cal text-xl tracking-wide">{title}</h3>
-                                        <p className="text-md italic text-gray-600 my-2">{description}</p>
-                                        <a href={link} className=" bg-white  rounded-full border border-gray-200 hover:border-black py-2.5 text-center w-full flex justify-center gap-5 mt-5">
+                                        <p className="text-md italic text-gray-600 my-2 h-[6em] md:h-[3em]">{description}</p>
+                                        <a href={link} className=" bg-white  rounded-full border border-gray-300 hover:border-black py-2.5 text-center w-full flex justify-center gap-5 mt-5">
                                             <p className="text-black">{linkText}</p>
                                             <Image src="/arrow.svg" alt={linkText} width={24} height={24} className="no-select-or-drag w-6" />
                                         </a>
--- constants/constants.tsx
+++ constants/constants.tsx
@@ -8,21 +8,21 @@ export const resources = [
         linkText: "Watch Video",
     },
     {
-        title: "Blockchain Bootcamp",
-        description: "This course will give you a full introduction into all of the core concepts related to blockchain.",
+        title: "Ethereum Whitepaper",
+        description: "This introductory paper was originally published in 2014 by Vitalik Buterin, the founder of Ethereum.",
         link: "https://www.youtube.com/watch?v=gyMwXuJrbJQ",
+        linkText: "Read Whitepaper",
+    },
+    {
+        title: "But how does bitcoin actually work?",
+        description: "The math behind cryptocurrencies, by 3Blue1Brown.",
+        link: "https://www.youtube.com/watch?v=bBC-nXj3Ng4",
         linkText: "Watch Video",
     },
     {
-        title: "Blockchain Bootcamp",
-        description: "This course will give you a full introduction into all of the core concepts related to blockchain.",
-        link: "https://www.youtube.com/watch?v=gyMwXuJrbJQ",
-        linkText: "Watch Video",
-    },
-    {
-        title: "Blockchain Bootcamp",
-        description: "This course will give you a full introduction into all of the core concepts related to blockchain.",
-        link: "https://www.youtube.com/watch?v=gyMwXuJrbJQ",
-        linkText: "Watch Video",
+        title: "Bitcoin Whitepaper",
+        description: "The original Bitcoin whitepaper, which started it all.",
+        link: "https://bitcoin.org/bitcoin.pdf",
+        linkText: "Read Whitepaper",
     },
 ]
